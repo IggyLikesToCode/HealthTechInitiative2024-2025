1ff0b84bed65ec59065602e30eb44ef4e4a90493
SahilVora82 <sahilvora828@gmail.com>
2024-12-27 17:32:33 -0500

    Update workspace.xml

--- hti24-25/src/App.js
+++ hti24-25/src/App.js
@@ -1,5 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server is running on port 3000');
+});
 
 import React from "react";
 import Navbar from "./components/navbar";
