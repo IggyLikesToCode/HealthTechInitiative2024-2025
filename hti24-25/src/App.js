90bf81bd3f58536544dc02ad85aa4100f66a8241
SahilVora82 <sahilvora828@gmail.com>
2024-12-27 17:37:21 -0500

    changed to app.js and server.js

--- hti24-25/src/App.js
+++ hti24-25/src/App.js
@@ -1,8 +1,5 @@
 import logo from "./logo.svg";
 import "./App.css";
-app.listen(3000, '0.0.0.0', () => {
-  console.log('Server is running on port 3000');
-});
 
 import React from "react";
 import Navbar from "./components/navbar";
