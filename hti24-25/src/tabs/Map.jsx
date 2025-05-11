46e9c5aae320114b4e499668de00381719f4fd09
SahilVora82 <sahilvora828@gmail.com>
2025-05-10 20:59:50 -0400

    fix

--- hti24-25/src/tabs/Map.jsx
+++ hti24-25/src/tabs/Map.jsx
@@ -19,7 +19,7 @@ function Map() {
     const BASE_URL =
         window.location.hostname === "localhost"
             ? "http://localhost:3001"
-            : "http://3.149.29.241/:3001";
+            : "http://3.149.29.241:3001";
 
     useEffect(() => {
         const fetchAll = async () => {
