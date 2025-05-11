ae8f3b29258064b9e0f5f840498aa3af3166577d
SahilVora82 <sahilvora828@gmail.com>
2025-05-10 20:56:06 -0400

    fixing ip for db connection

--- hti24-25/src/tabs/Report.jsx
+++ hti24-25/src/tabs/Report.jsx
@@ -16,7 +16,7 @@ import { CameraOutlined as Camera } from "@ant-design/icons";
 const BASE_URL =
     window.location.hostname === "localhost"
         ? "http://localhost:3001"
-        : "http://3.149.241.113:3001"; // your EC2 IP or domain here
+        : "http://3.149.29.241:3001"; // your EC2 IP or domain here
 
 const Report = () => {
   const [location, setLocation] = useState("");
